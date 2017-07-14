6600e407e2ec53f301401c4306f477ffcfb1182a
bpscrm <liyayacat@hotmail.com>
2017-07-14 09:23:11 +0800

    added to parse button definition xml file to menu json object

---
+++ src/com/bp/wei/util/MenuUtil.java
@@ -0,0 +1,36 @@
+package com.bp.wei.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import net.sf.json.JSON;
+import net.sf.json.xml.XMLSerializer;
+
+public class MenuUtil {
+	private static Logger log = LoggerFactory.getLogger(MenuUtil.class);
+	
+	public static String getMenuDefinition(){
+		Resource rs = new ClassPathResource("menu.xml");		
+		if (!rs.exists()) {
+			log.error("Menu definition file not found!");
+			return null;
+		}
+		XMLSerializer xmlSerializer = new XMLSerializer();
+		
+		try {
+			JSON jmenu = xmlSerializer.readFromStream(rs.getInputStream());	
+			
+			return jmenu.toString();
+		} catch (Exception e) {
+			log.error("Failed to read menu definition file: " + e.getMessage());
+			e.printStackTrace();
+		}		
+		return null;
+	}
+	
+	public static void main(String[] args){
+		MenuUtil.getMenuDefinition();
+	}
+}
